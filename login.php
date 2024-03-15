7d73f36833836afabd4c44129700e900a12fcddc
artem <artemfr@hotmail.com>
2024-03-15 20:09:40 +0100

    Adding verifications to login and index

--- login.php
+++ login.php
@@ -6,6 +6,19 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $sql = sprintf("SELECT * FROM user
                             WHERE email = '%s'",
                             $mysqli->real_escape_string($_POST["email"]));
+$result = $mysqli->query($sql);
+
+$user = $result->fetch_assoc();
+
+if ($user) {
+    if (password_verify($_POST["password"], $user["password_hash"])) {
+        session_start();
+        $_SESSION["user_id"] = $user["id"];
+        header("Location: index.php");
+        exit;
+    }
+}
+$is_invalid = true;
 }
 
 ?>
@@ -19,9 +32,15 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 </head>
 <body>
     <h1>Login</h1>
-    <form action="">
+
+    <?php if ($is_invalid) : ?>
+        <em> Invalid login</em>
+        <?php endif; ?>
+
+    <form method="post">
         <label for="email">Email</label>
-        <input type="email" name="email" id="email">
+        <input type="email" name="email" id="email"
+        value="<?= htmlspecialchars($_POST["email"] ?? "") ?>">
 
         <label for="password">Password</label>
         <input type="password" name="password" id="password">
