2ea2d567c7a0cd71c28d1e1c68c801941f7c418b
artem <artemfr@hotmail.com>
2024-03-15 14:14:45 +0100

    Added gitignore with database info and added additional signup process details

--- process-signup.php
+++ process-signup.php
@@ -26,5 +26,22 @@ if ($_POST["password"] !== $_POST["password_confirmation"]) {
 
 $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
-print_r($_POST);
-var_dump($password_hash);
+$mysqli = require __DIR__ . "/database.php";
+
+$sql = "INSERT INTO user (name, email, password_hash)
+            VALUES (?, ?, ?)";
+
+$stmt = $mysqli->stmt_init();
+
+if ( ! $stmt->prepare($sql)) {
+    die("SQL error:" . $mysqli->error);
+}
+
+$stmt->bind_param("sss", 
+                                $_POST["name"],
+                                $_POST["email"],
+                                $password_hash);
+if ($stmt->execute()) {
+    echo"Signup successful";
+};
+
